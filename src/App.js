ba79419aafd5a27d94ceb898c7454bb6ba294223
stephan.orlowsky <stephan.orlowsky@ibm.com>
2022-06-10 13:53:12 +0200

    (not) working examples

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import {MultiSelect} from '@carbon/react'
+import FilterableMultiSelect from "@carbon/react/lib/components/MultiSelect/FilterableMultiSelect";
+
+const items = [
+  {
+    id: 'downshift-1-item-0',
+    text: 'Option 1'
+  },
+  {
+    id: 'downshift-1-item-1',
+    text: 'Option 2'
+  },
+  {
+    disabled: true,
+    id: 'downshift-1-item-2',
+    text: 'Option 3 - a disabled item'
+  },
+  {
+    id: 'downshift-1-item-3',
+    text: 'Option 4'
+  },
+  {
+    id: 'downshift-1-item-4',
+    text: 'An example option that is really long to show what should be done to handle long text'
+  },
+  {
+    id: 'downshift-1-item-5',
+    text: 'Option 5'
+  }
+]
 
 function App() {
+  const [selectedItems, setSelectedItems] = useState([])
+
+  useEffect(() => {
+    setSelectedItems([
+      {
+        "id": "downshift-1-item-0",
+        "text": "Option 1"
+      },
+      {
+        "id": "downshift-1-item-1",
+        "text": "Option 2"
+      }
+    ])
+
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <MultiSelect
+        label=""
+        id="carbon-multiselect-example-3"
+        itemToString={(item) => item.text}
+        items={items}
+        selectedItems={selectedItems} //selectedItems works with state changes
+        titleText="MultiSelect is working"
+      />
+      <FilterableMultiSelect
+        placeholder=""
+        id="carbon-multiselect-example-3"
+        itemToString={(item) => item.text}
+        items={items}
+        initialSelectedItems={selectedItems} //this doesnt do anything
+        selectedItems={selectedItems} //this doesnt do anything
+        titleText="FilterableMultiSelect is not working"
+      />
+      <MultiSelect.Filterable
+        id="carbon-multiselect-example-3"
+        itemToString={(item) => item.text}
+        items={items}
+        initialSelectedItems={selectedItems} //this doesnt do anything
+        selectedItems={selectedItems} //this doesnt do anything
+        titleText="MultiSelect.Filterable is not working"
+      />
+    </>
+  )
 }
 
 export default App;
